a882f460ab55683c73828c471d1720efbcf19f7d
Jason Maina <auravision.africa@gmail.com>
2021-09-27 08:52:11 +0300

    Full project with 2D character control using Unity's new input system

---
+++ Assets/Aura/Scripts/PlatformActions.cs
@@ -0,0 +1,183 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Aura/Scripts/PlatformActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlatformActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlatformActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlatformActions"",
+    ""maps"": [
+        {
+            ""name"": ""Land"",
+            ""id"": ""eb812539-18fa-413f-a04d-e410d36bf5cc"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""c75c205f-957e-450d-bc62-5ef7c4e64650"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""e5c7e0eb-d621-4102-b57b-cd70526e695e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Sideways"",
+                    ""id"": ""92146609-6c2a-4b79-b5bc-77f7bdc428ed"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ca547e9b-f494-4907-bcaa-88cca4287079"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""65c5f5de-91a4-49a7-a6f2-6707cf3f8bb9"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""20584116-6f70-43eb-97d4-ef294c17ce86"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Land
+        m_Land = asset.FindActionMap("Land", throwIfNotFound: true);
+        m_Land_Move = m_Land.FindAction("Move", throwIfNotFound: true);
+        m_Land_Jump = m_Land.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Land
+    private readonly InputActionMap m_Land;
+    private ILandActions m_LandActionsCallbackInterface;
+    private readonly InputAction m_Land_Move;
+    private readonly InputAction m_Land_Jump;
+    public struct LandActions
+    {
+        private @PlatformActions m_Wrapper;
+        public LandActions(@PlatformActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Land_Move;
+        public InputAction @Jump => m_Wrapper.m_Land_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Land; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LandActions set) { return set.Get(); }
+        public void SetCallbacks(ILandActions instance)
+        {
+            if (m_Wrapper.m_LandActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_LandActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public LandActions @Land => new LandActions(this);
+    public interface ILandActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Aura/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlatformActions pActions;
+
+    [Header("Movement Variables")]
+    [SerializeField] float m_Speed, m_JumpSpeed;
+
+    [SerializeField]LayerMask groundMask;
+    Collider2D m_Collider;
+    Rigidbody2D m_RigidBody;
+
+    private void Awake()
+    {
+        pActions = new PlatformActions();
+        pActions.Land.Jump.performed += _=> Jump();
+
+        m_Collider = GetComponent<Collider2D>();
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Jump()
+    {
+        //add a force to players rigidbody if player is grounded
+        if(IsGrounded())
+        {
+            m_RigidBody.AddForce(Vector2.up * m_JumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        //Use collider to get player bounds
+        //check if the bounds overlap the ground
+        //if they do then IsGrounded is true
+        Vector2 topLeftCorner = transform.position;
+        Vector2 bottomRightCorner = transform.position;
+
+        topLeftCorner.x  -= m_Collider.bounds.size.x;
+        topLeftCorner.y -= m_Collider.bounds.size.y;
+
+        bottomRightCorner.x += m_Collider.bounds.size.x;
+        bottomRightCorner.y += m_Collider.bounds.size.y;
+
+        return Physics2D.OverlapArea(topLeftCorner, bottomRightCorner, groundMask);
+       
+    }
+
+    private void OnEnable()
+    {
+        pActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pActions.Disable();
+    }
+
+    private void Update()
+    {
+        //Read the movement value
+        float movementInput = pActions.Land.Move.ReadValue<float>();
+
+        //Move the player
+        Vector3 currentPosition = transform.position;
+        currentPosition.x += movementInput * m_Speed * Time.deltaTime;
+        transform.position = currentPosition;
+    }
+}
